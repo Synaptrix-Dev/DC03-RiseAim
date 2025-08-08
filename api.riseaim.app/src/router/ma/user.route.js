7d0a4444593db383979d8a6c02970101337ba91f
Synaptrix Solution <synaptrixsol@gmail.com>
2025-08-08 22:06:06 +0500

    updated

--- api.riseaim.app/src/router/ma/user.route.js
+++ api.riseaim.app/src/router/ma/user.route.js
@@ -1,13 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
-import protectAPIsMiddleware from "../../middleware/protectAPI.middleware.js";
+// import protectAPIsMiddleware from "../../middleware/protectAPI.middleware.js";
 import verifyToken from "../../middleware/token.middleware.js";
 import userController from "../../controller/ma/user.controller.js";
 
 const router = express.Router();
 
 router.use(bodyParser.json());
-router.use(protectAPIsMiddleware);
+// router.use(protectAPIsMiddleware);
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(verifyToken);
 
